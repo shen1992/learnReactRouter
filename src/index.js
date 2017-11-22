410e3215f1990a8b6b29d563c90607d5914d5395
shenyuan <shenyuan@tongbanjie.com>
2017-11-22 12:17:59 +0800

    删除无用文件

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React, { PropTypes, Component } from 'react'
 import ReactDOM from 'react-dom'
-import logo from './logo.svg';
 
 let instances = []
 const register = (comp) => instances.push(comp)
